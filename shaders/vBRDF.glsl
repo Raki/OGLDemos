f44af837f183cc135b9f1c3f15b8b43fc7859db7
Raki <824691+Raki@users.noreply.github.com>
2023-05-10 22:18:54 -0600

    PBR Complete

---
+++ shaders/vBRDF.glsl
@@ -0,0 +1,12 @@
+#version 460
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 aTexCoords;
+
+out vec2 TexCoords;
+
+void main()
+{
+    TexCoords = aTexCoords;
+	gl_Position = vec4(position, 1.0);
+}
